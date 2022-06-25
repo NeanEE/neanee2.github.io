ad5f6c6428ee4f03ec83c221f765b47c08d9c60c
NeanEE <96081010+NeanEE@users.noreply.github.com>
2022-06-24 22:15:21 -0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -47,7 +47,7 @@ slider_click.addEventListener("click", ()=>{
     // if (slidePosition == 4){
       // slider_modal.href = "ACA VA UN LINK DIRECTO A LA PAGINA DENTRO DE DECAP"
     // }
-    slider_ads_text.textContent = `Usted esta a punto de salir de Decap y dirigirse a "${adsLink}" proporcionado por: "${adsName}"`
+    slider_ads_text.textContent = `Usted esta a punto de salir de Deeee y dirigirse a "${adsLink}" proporcionado por: "${adsName}"`
     if (slidePosition <= 3){
     slider_modal.classList.add("slider__ads__modal__container--show");
     }
